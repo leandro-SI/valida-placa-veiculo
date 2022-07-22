edc940499cf15b4eac878d97e1584e6430553d08
Leandro Cesar <leandro.cesar@globalsys.com.br>
2022-07-22 14:18:24 -0300

    código de validação

---
+++ ValidaPlacaVeiculo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ValidaPlacaVeiculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite a Placa do Veículo:");
+            var placa = Console.ReadLine();
+
+            var resultado = Valida.ValidarPlaca(placa);
+            var mensagem = resultado ? "é válida" : "não é válida";
+
+            Console.WriteLine($"A placa informada {mensagem}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
